f1dea41a711d07c15eb864b1912c566c6c8cab07
Knevels1 <kyle.a.nevels@icloud.com>
2020-11-07 20:07:39 -0600

    created a return fetch method post to post to the local api with a dispatch change event

--- scripts/JournalEntry/JournalDataProvider.js
+++ scripts/JournalEntry/JournalDataProvider.js
@@ -1,12 +1,9 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
+const eventHub = document.querySelector(".container")
 
-// This is the original data.
+const dispatchStateChangeEvent = () => {
+    const entryStateChangeEvent = new CustomEvent("entryStateChanged")
+    eventHub.dispatchEvent(entryStateChangeEvent)
+}
 let allEntries = []
 
 export const useEntries = () => allEntries.slice()
@@ -15,13 +12,19 @@ export const useEntries = () => allEntries.slice()
 export const getEntries = () => {
     return fetch("http://localhost:8088/entries") // Fetch from the API
         .then(response => response.json())  // Parse as JSON
+        // What should happen when we finally have the array?
         .then(parsedEntries => {
             allEntries = parsedEntries
-            // What should happen when we finally have the array?
         })
 }
-
-/*
-    You export a function that provides a version of the
-    raw data in the format that you want
-*/
+export const saveEntry = (entry) => {
+    return fetch ('http://localhost:8088/entries', {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(entry)
+    })
+    .then(getEntries)
+    .then(dispatchStateChangeEvent)
+}
